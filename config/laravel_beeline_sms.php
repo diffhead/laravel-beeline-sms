7820ff5e6f111098714f9fe3df57dfd2b233a7b5
Kirill Zolotarev <saintsample36@gmail.com>
2024-02-01 19:21:51 +0300

    Fill config

--- config/laravel_beeline_sms.php
+++ config/laravel_beeline_sms.php
@@ -1,5 +1,14 @@
 <?php
 
 return [
-  ''
+  'api_host' => env('LARAVEL_BEELINE_API_HOST'),
+  'login' => env('LARAVEL_BEELINE_API_HOST'),
+  'pass' => env('LARAVEL_BEELINE_API_HOST'),
+  'gzip' => env('LARAVEL_BEELINE_API_HOST'),
+  'comment' => env('LARAVEL_BEELINE_API_HOST'),
+  'api_host' => env('LARAVEL_BEELINE_API_HOST'),
+  'api_host' => env('LARAVEL_BEELINE_API_HOST'),
+  'api_host' => env('LARAVEL_BEELINE_API_HOST'),
+  'api_host' => env('LARAVEL_BEELINE_API_HOST'),
+  'api_host' => env('LARAVEL_BEELINE_API_HOST'),
 ];
